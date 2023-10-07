3753c1b98a98fe5dbf1e7d507289152ba6bcc944
janidulnkg1 <janidulnkg1@gmail.com>
2023-10-07 23:27:47 +0530

    Add changes

--- SBT-RMEx-Automation/SBT-RMEx-Automation/Program.cs
+++ SBT-RMEx-Automation/SBT-RMEx-Automation/Program.cs
@@ -4,77 +4,68 @@ using SBT_RMEx_Automation.ClientBuilder;
 using Serilog;
 using System.Text.Json;
 
-namespace CsvFileTransferApp
-{
-    class Program
-    {
-        static void Main()
-        {
-            Log.Logger = new LoggerConfiguration()
+Log.Logger = new LoggerConfiguration()
                 .MinimumLevel.Information()
                 .WriteTo.Console()
                 .WriteTo.File("/logs/SBT-RMEx_{Date}.log", rollingInterval: RollingInterval.Day)
                 .CreateLogger();
 
-            string clientsJsonFile = "clientDetails.json";
-            DateTime currentDate = DateTime.Now.Date;
+string clientsJsonFile = "clientDetails.json";
+DateTime currentDate = DateTime.Now.Date;
 
-            try
+try
+{
+    var clients = JsonSerializer.Deserialize<Client[]>(File.ReadAllText(clientsJsonFile));
+
+    foreach (var clientInfo in clients)
+    {
+        // Using ClientBuilder to create a Client object
+        IClientBuilder clientBuilder = new ClientBuilder()
+            .WithIpAddress(clientInfo.IpAddress)
+            .WithUsername(clientInfo.Username)
+            .WithPassword(clientInfo.Password)
+            .WithSourceDirectory(clientInfo.SourceDirectory)
+            .WithDestinationDirectory(clientInfo.DestinationDirectory);
+
+        Client client = clientBuilder.Build();
+
+        DirectoryInfo dirInfo = new DirectoryInfo(client.SourceDirectory);
+
+        // Getting all .csv files in the source directory created on the current date
+        var csvFiles = dirInfo.GetFiles("*.csv")
+            .Where(file => file.CreationTime.Date == currentDate);
+
+        using (var sftpClient = new SftpClient(client.IpAddress, client.Username, client.Password))
+        {
+            sftpClient.Connect();
+            sftpClient.ChangeDirectory(client.DestinationDirectory);
+
+            foreach (var csvFile in csvFiles)
             {
-                var clients = JsonSerializer.Deserialize<Client[]>(File.ReadAllText(clientsJsonFile));
-
-                foreach (var clientInfo in clients)
+                using (var fileStream = File.OpenRead(csvFile.FullName))
                 {
-                    // Using ClientBuilder to create a Client object
-                    IClientBuilder clientBuilder = new ClientBuilder()
-                        .WithIpAddress(clientInfo.IpAddress)
-                        .WithUsername(clientInfo.Username)
-                        .WithPassword(clientInfo.Password)
-                        .WithSourceDirectory(clientInfo.SourceDirectory)
-                        .WithDestinationDirectory(clientInfo.DestinationDirectory);
-
-                    Client client = clientBuilder.Build();
-
-                    DirectoryInfo dirInfo = new DirectoryInfo(client.SourceDirectory);
-
-                    // Getting all .csv files in the source directory created on the current date
-                    var csvFiles = dirInfo.GetFiles("*.csv")
-                        .Where(file => file.CreationTime.Date == currentDate);
-
-                    using (var sftpClient = new SftpClient(client.IpAddress, client.Username, client.Password))
-                    {
-                        sftpClient.Connect();
-                        sftpClient.ChangeDirectory(client.DestinationDirectory);
-
-                        foreach (var csvFile in csvFiles)
-                        {
-                            using (var fileStream = File.OpenRead(csvFile.FullName))
-                            {
-                                sftpClient.UploadFile(fileStream, csvFile.Name);
-                            }
-
-                            // Moving to a different directory as backup
-                            string processedDirectory = Path.Combine(client.SourceDirectory, "Processed");
-                            Directory.CreateDirectory(processedDirectory);
-                            File.Move(csvFile.FullName, Path.Combine(processedDirectory, csvFile.Name));
-
-                            Log.Information($"Transferred: {csvFile.Name}");
-                        }
-
-                        sftpClient.Disconnect();
-                    }
+                    sftpClient.UploadFile(fileStream, csvFile.Name);
                 }
 
-                Log.Information("All .csv files transferred successfully.");
+                // Moving to a different directory as backup
+                string processedDirectory = Path.Combine(client.SourceDirectory, "Processed");
+                Directory.CreateDirectory(processedDirectory);
+                File.Move(csvFile.FullName, Path.Combine(processedDirectory, csvFile.Name));
+
+                Log.Information($"Transferred: {csvFile.Name}");
             }
-            catch (Exception ex)
-            {
-                Log.Error(ex, $"Error: {ex.Message}");
-            }
-            finally
-            {
-                Log.CloseAndFlush();
-            }
+
+            sftpClient.Disconnect();
         }
     }
+
+    Log.Information("All .csv files transferred successfully.");
+}
+catch (Exception ex)
+{
+    Log.Error(ex, $"Error: {ex.Message}");
+}
+finally
+{
+    Log.CloseAndFlush();
 }
